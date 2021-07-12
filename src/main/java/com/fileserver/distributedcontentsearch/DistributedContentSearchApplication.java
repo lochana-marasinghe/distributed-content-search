45fc74a0a6906725dbbb2a8e6209a00fd5d4686e
Bathiya Thennakoon <bathiya-94>
2021-07-12 07:12:11 +0530

    Initial commit

---
+++ src/main/java/com/fileserver/distributedcontentsearch/DistributedContentSearchApplication.java
@@ -0,0 +1,13 @@
+package com.fileserver.distributedcontentsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedContentSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedContentSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fileserver/distributedcontentsearch/DistributedContentSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileserver.distributedcontentsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedContentSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
